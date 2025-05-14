ee3d118d700084257f19db1e52144d5497808f86
ghitchmansmith <114439927+ghitchmansmith@users.noreply.github.com>
2025-05-14 14:42:50 +0100

    Initial Commit

---
+++ OneNews/OneNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneNewsApp.swift
+//  OneNews
+//
+//  Created by George Hitchman-Smith on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct OneNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
